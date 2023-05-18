6976e1d905c88d7e84f0126f4852b0ef651f9da5
Victor Mafra <victorvmafra97@gmail.com>
2023-05-18 15:19:54 -0300

    added users

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,18 @@
+import { db } from "../database/database.connection.js"
+import {v4 as uuid} from "uuid"
+import bcrypt from "bcrypt"
+
+export async function singup(req, res){
+    const {name,  email, password, confirmPassword} = req.body
+
+    if(password !== confirmPassword) return res.status(422).send("Passwords don't match")
+
+    const emailExist = await db.query(`SELECT * FROM users WHERE email=$1;`, [email])
+    if(emailExist.rows[0].length > 0) return res.status(409).send("Email already registered")
+
+    try{
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
