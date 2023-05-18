038c93c738bacd0116a34f26d39d9d363514b65b
Victor Mafra <victorvmafra97@gmail.com>
2023-05-18 15:22:17 -0300

    some adjustment to deploy

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import express from "express"
 import cors from "cors"
-import router from "./routes/index.routes.js"
 import dotenv from "dotenv"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(router)
 dotenv.config()
 
 const port = process.env.PORT || 5000
