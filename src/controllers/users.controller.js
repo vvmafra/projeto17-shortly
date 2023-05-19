f61621cc88a6fcfa8563d40deebc97772b0e6ce0
Victor Mafra <victorvmafra97@gmail.com>
2023-05-18 21:10:24 -0300

    login almost finished

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,7 +2,7 @@ import { db } from "../database/database.connection.js"
 import {v4 as uuid} from "uuid"
 import bcrypt from "bcrypt"
 
-export async function singup(req, res){
+export async function signup(req, res){
     const {name,  email, password, confirmPassword} = req.body
 
     if(password !== confirmPassword) return res.status(422).send("Passwords don't match")
@@ -18,4 +18,24 @@ export async function singup(req, res){
     } catch (err) {
         res.status(500).send(err.message)
     }
+}
+
+export async function signin(req, res){
+    const {email, password} = req.body
+
+    const emailExist = await db.query(`SELECT * FROM users WHERE email=$1;`, [email])
+    if(emailExist.rows.length === 0) return res.status(401).send("Email not registered")
+
+    const checkPassword = bcrypt.compareSync(password, emailExist.rows[0].password)
+    if(!checkPassword) return res.status(401).send("Password doesn't match with email")
+
+    try{
+        const token = uuid()
+        await db.query(``)
+        res.status(200).send(token)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { singup } from "../controllers/users.controller.js";
+import { signin, signup } from "../controllers/users.controller.js";
 import { validateSchemas } from "../middlewares/validateSchema.middleware.js";
-import { userSchema } from "../schemas/users.schemas.js";
+import { loginSchema, userSchema } from "../schemas/users.schemas.js";
 
 const userRouter = Router()
 
-userRouter.post("/signup", validateSchemas(userSchema),singup)
+userRouter.post("/signup", validateSchemas(userSchema),signup)
+userRouter.post("/signin", validateSchemas(loginSchema), signin)
 
 export default userRouter
