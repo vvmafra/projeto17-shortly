53a5b256d783dd9ea4b572f84a5d6a188f75a1a1
Victor Mafra <victorvmafra97@gmail.com>
2023-05-20 13:01:09 -0300

    testing nanoid

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -5,15 +5,17 @@ export async function postUrls(req, res){
     const {url} = req.body
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "")
+    const { nanoid } = require("nanoid/non-secure")
+
 
     try {
         const sessions = await db.query(`SELECT * FROM logins WHERE token=$1`, [token])
         if (sessions.rows.length === 0) return res.sendStatus(401)
 
         const userSession = await db.query(`SELECT * FROM users WHERE "id"=$1`,[sessions.rows[0].idUser])
-        const alphanumeric = require('nanoid-generator/alphanumeric');
+        shortUrl = nanoid()
 
-        console.log(alphanumeric)
+        console.log(shortUrl)
         // await db.query(`INSERT INTO urls ("idUser", "idLogin", url, "shortUrl", views, "createdAt") VALUES ($1, $2, $3, $4, $5, $6)`,[])
 
         res.sendStatus(201)
