57766ed030887f043724b1dad50f51694360aa0e
Victor Mafra <victorvmafra97@gmail.com>
2023-05-22 12:47:23 -0300

    adjust visitCount getOpenUrl

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -56,7 +56,7 @@ export async function getOpenUrl(req, res){
     if (urlFind.rows.length === 0) return res.sendStatus(404)
 
     try {
-        await db.query(`UPDATE urls SET visitCount = visitCount + 1 WHERE "shortUrl"=$1;`, [shortUrl])
+        await db.query(`UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE "shortUrl"=$1;`, [shortUrl])
         console.log(urlFind.rows[0])
         res.redirect(302, urlFind.rows[0].url)
     } catch (err) {
